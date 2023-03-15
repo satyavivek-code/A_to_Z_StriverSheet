4ac58a7514a12102df09c612e2e793f7481dec85
Satya Vivek Jaiswal <125399156+satyavivek-code@users.noreply.github.com>
2023-03-15 09:26:58 +0530

    Update Largest Element in Array.cpp

--- Largest Element in Array.cpp
+++ Largest Element in Array.cpp
@@ -8,3 +8,28 @@ int largest(vector<int> &arr, int n)
   sort(arr.begin(), arr.end());
   return (arr[n-1]);
 }
+
+
+
+/*
+VS code
+*/
+
+#include<iostream>
+using namespace std;
+int maxValue (int arr[], int n){
+    int max = arr[0];
+    for (int i = 0; i < n; i++){
+        if (max < arr[i]){
+            max = arr[i];
+        }
+    }
+    return max;
+}
+int main()
+{
+    int arr[5] = {5,6,8,1,2};
+    int max = maxValue(arr, 5);
+    cout << "The largest element in array is : "<< max << endl;
+    return 0;
+}
