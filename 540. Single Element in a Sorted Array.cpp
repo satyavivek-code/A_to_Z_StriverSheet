c9d24c27be4ee4c9448855a3a5f478f570298e80
Satya Vivek Jaiswal <125399156+satyavivek-code@users.noreply.github.com>
2023-04-21 20:24:57 +0530

    Create 540. Single Element in a Sorted Array.cpp

---
+++ 540. Single Element in a Sorted Array.cpp
@@ -0,0 +1,14 @@
+/*
+LeetCode Solution
+*/
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int xOR = 0;
+        for (int i = 0; i <nums.size(); i++){
+            xOR^=nums[i];
+        }
+        return xOR;
+    }
+};
