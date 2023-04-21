57fda0797445d83f76f3f44e89c342697668b933
Satya Vivek Jaiswal <125399156+satyavivek-code@users.noreply.github.com>
2023-04-21 13:41:49 +0530

    Update 153. Find Minimum in Rotated Sorted Array.cpp

--- 153. Find Minimum in Rotated Sorted Array.cpp
+++ 153. Find Minimum in Rotated Sorted Array.cpp
@@ -31,3 +31,29 @@ public:
         return minValue;
     }
 };
+
+/*
+Find minimum element of the rotated sorted array  part 2
+*/
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int start = 0;
+        int end = nums.size()-1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] < nums[end]){
+                end = mid ;
+            }
+            else if (nums[mid] > nums[end]){
+                start = mid + 1;
+            }
+            else{
+                end--;
+            }
+        }
+        return nums[start];
+    }
+};
